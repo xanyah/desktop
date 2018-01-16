c75bacfebf994ffae8ca76f55e2facbd964c5313
Sofiane Gargouri <contact@sofianeg.com>
2018-01-16 16:31:43 +0100

    Adding Building configuration

--- installers/osx.js
+++
@@ -1,7 +0,0 @@
-var createDMG = require('electron-installer-dmg')
-
-createDMG({
-  appPath: './builds/Xanyah-darwin-x64/Xanyah.app',
-  name: 'Xanyah',
-  out: './installers/builds',
-}, err => console.log(err))
