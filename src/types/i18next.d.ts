984a4f09981ef4735fec18585630f0b3a28fb4ab
Sofiane <contact@sofianeg.com>
2025-02-02 17:08:08 +0100

    Fixing TS

--- src/types/i18next.d.ts
+++ src/types/i18next.d.ts
@@ -1,9 +1,12 @@
-import 'i18next'
 import fr from '../i18n/dictionaries/fr.json'
+import zodFr from 'zod-i18n-map/locales/fr/zod.json'
 
 declare module 'i18next' {
   interface CustomTypeOptions {
-    defaultNS: string
-    resources: typeof fr
+    defaultNS: 'translation'
+    resources: {
+      translation: typeof fr,
+      zod: typeof zodFr,
+    }
   }
 }
