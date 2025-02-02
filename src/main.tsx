59383d2573e7d0293c1b93065ea2e63a57acef6e
Sofiane <contact@sofianeg.com>
2025-02-02 17:49:37 +0100

    Adding Sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,18 @@
+import * as Sentry from '@sentry/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app'
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0,
+  replaysOnErrorSampleRate: 1.0,
+})
 
 const root = document.getElementById('root')
 
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,26 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "sofiane-gargouri",
+    project: "xanyah-web"
+  })],
+
   define: {
     global: {},
   },
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+
+  build: {
+    sourcemap: true
+  }
 })
