650ce4b06971374e265640de1da32ce3985fb10d
Sofiane <contact@sofianeg.com>
2025-02-02 17:10:47 +0100

    Fixing ESLint

--- src/types/i18next.d.ts
+++ src/types/i18next.d.ts
@@ -5,8 +5,8 @@ declare module 'i18next' {
   interface CustomTypeOptions {
     defaultNS: 'translation'
     resources: {
-      translation: typeof fr,
-      zod: typeof zodFr,
+      translation: typeof fr
+      zod: typeof zodFr
     }
   }
 }
