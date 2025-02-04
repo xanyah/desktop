78431bb47158aa9a9bf64bb3747a7dd50dba7577
Sofiane <contact@sofianeg.com>
2025-02-04 12:12:45 +0100

    Fixing conflict

--- src/api/stores.ts
+++ src/api/stores.ts
@@ -9,6 +9,3 @@ export const getStoreMemberships = params =>
 
 export const updateStore = (storeId, params) =>
   xanyahApi.patch<Store>(`v2/stores/${storeId}`, decamelizeKeys(params))
-
-export const getStoreMemberships = params =>
-  xanyahApi.get<StoreMembership[]>('v2/store_memberships', decamelizeKeys({ params }))
