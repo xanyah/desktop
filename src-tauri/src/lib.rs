716af7e948d0b09828b9b0dc6c31faf5d4858e17
Sofiane <contact@sofianeg.com>
2025-02-05 14:04:18 +0100

    Adding

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,64 @@
+use printers::common::base::printer::PrinterState;
+use printers::get_default_printer as printers_get_default_printer;
+use printers::get_printer_by_name as printers_get_printer_by_name;
+use printers::get_printers as printers_get_printers;
+use printers::common::base::printer::Printer;
+use serde_json::json;
+use serde_json::Value;
+
+fn format_printer_state(printer_state: PrinterState) -> String {
+  return match printer_state{
+    PrinterState::READY=>"ready".to_owned(),
+    PrinterState::PAUSED=>"paused".to_owned(),
+    PrinterState::PRINTING=>"printing".to_owned(),
+    PrinterState::UNKNOWN=>"unknown".to_owned(),
+   };
+}
+
+fn format_printer(printer: Option<Printer>)-> Value {
+  if printer.is_some() {
+    let unwrapped_printer = printer.unwrap();
+    return json!({
+      "name": unwrapped_printer.name,
+      "system_name": unwrapped_printer.system_name,
+      "driver_name": unwrapped_printer.driver_name,
+      "uri": unwrapped_printer.uri,
+      "port_name": unwrapped_printer.port_name,
+      "processor": unwrapped_printer.processor,
+      "data_type": unwrapped_printer.data_type,
+      "description": unwrapped_printer.description,
+      "location": unwrapped_printer.location,
+      "is_default": unwrapped_printer.is_default,
+      "is_shared": unwrapped_printer.is_shared,
+      "state": format_printer_state(unwrapped_printer.state),
+    });
+  } else {
+    return json!(false);
+  }
+}
+
+#[tauri::command]
+fn get_printer_by_name(name: String) -> Value {
+  return format_printer(printers_get_printer_by_name(&name));
+}
+
+#[tauri::command]
+fn get_default_printer() -> Value {
+  return format_printer(printers_get_default_printer());
+}
+
+#[tauri::command]
+fn get_printers() -> impl Iterator<Item = serde_json::Value> {
+  return printers_get_printers().iter().map(|x| format_printer(Some(x.to_owned())));
+}
+
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![get_printer_by_name])
+    .invoke_handler(tauri::generate_handler![get_default_printer])
+    .invoke_handler(tauri::generate_handler![get_printers])
     .setup(|app| {
       if cfg!(debug_assertions) {
         app.handle().plugin(
