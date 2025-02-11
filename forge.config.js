9907c88c06fd7f2c0629975314b54f597f5ba5b8
Sofiane <contact@sofianeg.com>
2025-02-11 21:42:09 +0100

    Publishing version 0.7.1

--- forge.config.js
+++ forge.config.js
@@ -41,4 +41,16 @@ module.exports = {
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'xanyah',
+          name: 'desktop'
+        },
+        prerelease: true
+      }
+    }
+  ]
 };
